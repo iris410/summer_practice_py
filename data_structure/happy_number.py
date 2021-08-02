9cfcec851a49e987d5cbb2c1a07cfaf4b6ca578a
Jingyi Cui <jingyi.cui1@ucdconnect.ie>
2021-08-02 07:37:39 +0100

    hashset practice

--- data_structure/happy_number.py
+++ data_structure/happy_number.py
@@ -1,16 +1,19 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        digits_sum = 0
         s = set()
         while True:
+            digits_sum = 0
             while n > 0:
                 digit_num = n % 10
-                digits_sum = digits_sum + digit_num^2
-                n = n//10
+                digits_sum = digits_sum + digit_num**2
+                n = int(n/10)
             n = digits_sum
-            if n == 1:
+            if digits_sum == 1:
                 return True
             elif digits_sum in s:
                 return False
             else:
                 s.add(digits_sum)
+
+p = Solution()
+print(p.isHappy(19))
